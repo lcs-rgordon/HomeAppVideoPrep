459d124de339d57184cf40357b1fd61c1a49233e
Russell Gordon <rgordon@lcs.on.ca>
2024-01-14 09:25:35 -0500

    Created linear gradient for background.

--- HomeAppVideoPrep/Views/HomeView.swift
+++ HomeAppVideoPrep/Views/HomeView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    // MARK: Stored properties
+    let firstGradient = Gradient(colors: [.gradientBlue, .gradientPurple, .gradientYellow])
+    
+    // MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            LinearGradient(
+                gradient: firstGradient,
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+            
+        }
     }
 }
 

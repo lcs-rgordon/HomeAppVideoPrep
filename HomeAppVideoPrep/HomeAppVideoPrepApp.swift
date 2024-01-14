f54ed724c8e3d342971e5014c40faa5f8ed57045
Russell Gordon <rgordon@lcs.on.ca>
2024-01-14 09:04:41 -0500

    Initial Commit

---
+++ HomeAppVideoPrep/HomeAppVideoPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppVideoPrepApp.swift
+//  HomeAppVideoPrep
+//
+//  Created by Russell Gordon on 2024-01-14.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppVideoPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
